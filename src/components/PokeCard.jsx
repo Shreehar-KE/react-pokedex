cba0163c1689b7f6519591b75873671445699edd
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-10-21 20:23:15 +0530

    complete API fetch

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,4 +1,54 @@
-export function PokeCard() {
+import { useEffect, useState } from "react"
+import { getFullPokedexNumber, getPokedexNumber } from "../utils/";
+
+export function PokeCard(props) {
+
+  const { selectedPokemon } = props
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+
+
+  useEffect(() => {
+    if (loading || !localStorage) { return }
+
+    let cache = {}
+    if (localStorage.getItem('pokedex')) {
+      cache = JSON.parse(localStorage.getItem('pokedex'))
+    }
+
+    if (selectedPokemon in cache) {
+      setData(cache[selectedPokemon])
+      return
+    }
+
+    async function fetchPokemonData() {
+      setLoading(true)
+      try {
+        const baseUrl = 'https://pokeapi.co/api/v2/'
+        const suffix = 'pokemon/' + getPokedexNumber(selectedPokemon)
+        const finalUrl = baseUrl + suffix
+        const res = await fetch(finalUrl)
+        const pokemonData = await res.json()
+        setData(pokemonData)
+        console.log(pokemonData)
+
+        cache[selectedPokemon] = pokemonData
+        localStorage.setItem('pokedex', JSON.stringify(cache))
+      }
+      catch (error) {
+        console.log(error.message)
+      }
+      finally {
+        setLoading(false)
+      }
+    }
+    fetchPokemonData()
+
+  }, [selectedPokemon])
+
+
+
   return (
     <div></div>
   )
